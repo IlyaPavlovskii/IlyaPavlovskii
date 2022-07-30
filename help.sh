7c33d4ef1bdba9cb44198b55eac657da7a23c2b9
ipavlovskii <pavlovskii_ilya@mail.ru>
2022-07-30 13:52:52 +0200

    Migrate StopRefactoring english version.

--- help.sh
+++ help.sh
@@ -3,13 +3,12 @@ docker build -t mkdocs .
 
  # Docker run
 docker run -t --rm -d -v /Users/ipavlovskii/PycharmProjects/docker-sampl/:/project --name mkdocs-container mkdocs
+mkdocs build -f /project/blog/src/mkdocs.yml
 # Open shell mode
 docker exec -it mkdocs-container bash
 
-# Execute mkdocs rebuild
-  # in docker
-  cd project
-  mkdocs build
+mkdocs build -c -f /project/blog/src/mkdocs.yml
+cp -r /project/blog/src/site/* /project | rm -rf /project/blog/src/site
 
 cp -r ./blog/src/site/* .
 rm -rf ./blog/src/site
